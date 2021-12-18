bbbb689d108cadf14155ec6af1faf8208c11cfd1
Kopamed <kopamedve@gmail.com>
2021-12-18 20:29:41 +0100

    basics.. not even

---
+++ src/main/java/me/x150/sipprivate/feature/module/impl/render/Fullbright.java
@@ -0,0 +1,46 @@
+package me.x150.sipprivate.feature.module.impl.render;
+
+import jdk.jfr.Category;
+import me.x150.sipprivate.SipoverPrivate;
+import me.x150.sipprivate.feature.module.Module;
+import me.x150.sipprivate.feature.module.ModuleType;
+import me.x150.sipprivate.helper.util.Transitions;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.MathHelper;
+
+public class Fullbright extends Module {
+
+    double og;
+
+    public Fullbright() {
+        super("Fullbright", "shine bright like a diamond", ModuleType.RENDER);
+    }
+
+    @Override public void tick() {
+
+    }
+
+    @Override public void enable() {
+        og = MathHelper.clamp(SipoverPrivate.client.options.gamma, 0, 1);
+    }
+
+    @Override public void disable() {
+       SipoverPrivate.client.options.gamma = og;
+    }
+
+    @Override public void onFastTick() {
+       SipoverPrivate.client.options.gamma = Transitions.transition(SipoverPrivate.client.options.gamma, 10, 300);
+    }
+
+    @Override public String getContext() {
+        return null;
+    }
+
+    @Override public void onWorldRender(MatrixStack matrices) {
+
+    }
+
+    @Override public void onHudRender() {
+
+    }
+}
