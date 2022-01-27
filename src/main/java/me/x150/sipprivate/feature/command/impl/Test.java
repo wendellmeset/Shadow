1e89e2c6c4ac7683a203ac4b9ebfc2a2763ab0a4
0x151 <constantin.chris10@gmail.com>
2022-01-27 21:38:23 +0100

    me when i put

--- src/main/java/me/x150/sipprivate/feature/command/impl/Test.java
+++ src/main/java/me/x150/sipprivate/feature/command/impl/Test.java
@@ -3,6 +3,11 @@ package me.x150.sipprivate.feature.command.impl;
 import me.x150.sipprivate.feature.command.Command;
 import me.x150.sipprivate.feature.gui.notifications.Notification;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Control;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.Mixer;
+
 public class Test extends Command {
     public Test() {
         super("Test", "REAL", "test");
@@ -10,9 +15,10 @@ public class Test extends Command {
 
     @Override
     public void onExecute(String[] args) {
-        Notification.create(10000,"bruh", Notification.Type.INFO, "REALFDHJKFHDSJKFHSJKDFH KJDSHFJKDSHFKJDSHFJKDSaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaH FKJSDHJKFK HSDKJFHDSJKFHJKSD HJKFHDSJKF HDSJKFHSDJK HJKSDHF KSDHF KJDSHFKJSDH FJKSDHFJKS");
-        Notification.create(10000,"bruh", Notification.Type.WARNING, "REAL");
-        Notification.create(10000,"bruh", Notification.Type.SUCCESS, "REAL");
-        Notification.create(10000,"bruh", Notification.Type.ERROR, "REAL");
+//        Notification.create(10000,"bruh", Notification.Type.INFO, "REALFDHJKFHDSJKFHSJKDFH KJDSHFJKDSHFKJDSHFJKDSaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaH FKJSDHJKFK HSDKJFHDSJKFHJKSD HJKFHDSJKF HDSJKFHSDJK HJKSDHF KSDHF KJDSHFKJSDH FJKSDHFJKS");
+//        Notification.create(10000,"bruh", Notification.Type.WARNING, "REAL");
+//        Notification.create(10000,"bruh", Notification.Type.SUCCESS, "REAL");
+//        Notification.create(10000,"bruh", Notification.Type.ERROR, "REAL");
+
     }
 }
