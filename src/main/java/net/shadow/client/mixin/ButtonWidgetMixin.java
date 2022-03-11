ff5ed050dd15a42afeb3fb9700c6ef2f015a5864
Saturn5Vfive <luke.inventor@gmail.com>
2022-03-10 19:26:56 -0600

    simple buttons

---
+++ src/main/java/net/shadow/client/mixin/ButtonWidgetMixin.java
@@ -0,0 +1,50 @@
+package net.shadow.client.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.MathHelper;
+import net.shadow.client.helper.font.FontRenderers;
+import net.shadow.client.helper.render.Renderer;
+import net.shadow.client.helper.util.Transitions;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.awt.*;
+
+@Mixin(ClickableWidget.class)
+public abstract class ButtonWidgetMixin {
+    final Color unselectedColor = new Color(150, 150, 150, 75);
+    final Color selectedColor = new Color(100, 100, 100, 100);
+    @org.spongepowered.asm.mixin.Shadow
+    public int x;
+    @org.spongepowered.asm.mixin.Shadow
+    public int y;
+    @org.spongepowered.asm.mixin.Shadow
+    protected int width;
+    @org.spongepowered.asm.mixin.Shadow
+    protected int height;
+    double animProgress = 0;
+
+    @org.spongepowered.asm.mixin.Shadow
+    public abstract boolean isHovered();
+
+    @org.spongepowered.asm.mixin.Shadow
+    public abstract Text getMessage();
+
+    @Inject(method = "renderButton", at = @At("HEAD"), cancellable = true)
+    public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        //if (!UnloadModule.loaded) return;
+        Renderer.R2D.renderRoundedQuad(matrices, this.isHovered() ? selectedColor : unselectedColor, x, y, x + width, y + height, 5, 7);
+        FontRenderers.getRenderer().drawCenteredString(matrices, this.getMessage().getString(), this.x + this.width / 2f, this.y + (this.height - 9) / 2f, 0xFFFFFF);
+        ci.cancel();
+    }
+
+
+    double easeInOutQuint(double x) {
+        return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+    }
+}
