0f339b55d9b0976cac85e4a5883c9752414b282e
Saturn5Vfive <luke.inventor@gmail.com>
2022-03-19 12:36:10 -0500

    real

--- src/main/java/net/shadow/client/feature/module/impl/render/Hud.java
+++ src/main/java/net/shadow/client/feature/module/impl/render/Hud.java
@@ -7,6 +7,8 @@ package net.shadow.client.feature.module.impl.render;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.network.packet.s2c.play.WorldTimeUpdateS2CPacket;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
@@ -215,9 +217,9 @@ public class Hud extends Module {
 //        Renderer.R2D.renderRoundedQuad(ms, ThemeManager.getMainTheme().getInactive(), rootX + 1, rootY + i + 3, rootX + width, rootY + height + i + 3, 5, 11);
         RenderSystem.defaultBlendFunc();
         RenderSystem.enableBlend();
-
         double imgWidth = 507 / 5d;
         double imgHeight = 167 / 5d;
+        
         double widgetWidth = Math.max(Math.max(imgWidth, width), 160) + 6;
         double widgetHeight = 3 + imgHeight + 3 + FontRenderers.getRenderer().getMarginHeight() + 3;
         double widgetX = 0;
