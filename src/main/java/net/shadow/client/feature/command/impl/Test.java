9b2d702e5091595655a66ad6419d8b07d8ec0fc5
0x3C50 <constantindevalt@gmail.com>
2022-04-22 10:11:47 +0200

    remove whatthefuck

--- src/main/java/net/shadow/client/feature/command/impl/Test.java
+++ src/main/java/net/shadow/client/feature/command/impl/Test.java
@@ -4,16 +4,7 @@
 
 package net.shadow.client.feature.command.impl;
 
-import net.minecraft.item.ItemStack;
-import net.shadow.client.WhatTheFuck;
 import net.shadow.client.feature.command.Command;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.lang.reflect.Field;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.List;
 
 public class Test extends Command {
     public Test() {
@@ -22,26 +13,6 @@ public class Test extends Command {
 
     @Override
     public void onExecute(String[] args) {
-        WhatTheFuck.loadTags();
-        try {
-            File out = new File("nbt.shit");
-            FileOutputStream fos = new FileOutputStream(out);
-            for (Field declaredField : WhatTheFuck.class.getDeclaredFields()) {
-                if (declaredField.getType().equals(List.class)) {
-                    declaredField.setAccessible(true);
-                    List<ItemStack> l = (List<ItemStack>) declaredField.get(null);
-                    for (ItemStack stack : l) {
-                        String fName = stack.getItem().toString();
-                        String nbt = Base64.getEncoder().encodeToString(stack.getOrCreateNbt().toString().getBytes(StandardCharsets.UTF_8));
-                        String p = declaredField.getName() + ";" + fName + ";" + nbt;
-                        fos.write((p + "\n").getBytes(StandardCharsets.UTF_8));
-                    }
-
-                }
-            }
-            fos.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        success("jooj");
     }
 }
