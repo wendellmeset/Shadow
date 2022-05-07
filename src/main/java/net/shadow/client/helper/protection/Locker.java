291ca24f4aae57a69e52116cd6fb5fb70db1941a
ZeoNight <zeonightyt@gmail.com>
2022-05-07 16:40:07 +0000

    🔒️ Add locker

--- src/main/java/net/shadow/client/helper/protection/Locker.java
+++ src/main/java/net/shadow/client/helper/protection/Locker.java
@@ -2,13 +2,17 @@ package net.shadow.client.helper.protection;
 
 import meteordevelopment.discordipc.DiscordIPC;
 import meteordevelopment.discordipc.IPCUser;
+import java.util.Arrays;
+import java.util.List;
+import net.minecraft.client.MinecraftClient;
 
 public class Locker {
-
+    private List<String> permittedIds = Arrays.asList("661988735189254165","956655656704815124");
     public static void init() {
         boolean result = DiscordIPC.start(958479347390500874L, () -> {
             IPCUser user = DiscordIPC.getUser();
-            //user.id
+            if(!permittedIds.has(user.id)) MinecraftClient.getInstance().close();
+
             DiscordIPC.stop();
         });
         if (!result) {
