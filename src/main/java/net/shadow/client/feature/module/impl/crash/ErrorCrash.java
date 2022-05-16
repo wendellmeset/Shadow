f695a937e8c900101fd3a197375fd7faad2354f9
ZeoNight <me@zeonight.com>
2022-05-16 22:43:42 +0300

    i forgor

--- src/main/java/net/shadow/client/feature/module/impl/crash/ErrorCrash.java
+++ src/main/java/net/shadow/client/feature/module/impl/crash/ErrorCrash.java
@@ -29,7 +29,7 @@ public class ErrorCrash extends Module {
     final DoubleSetting pwr = this.config.create(new DoubleSetting.Builder(5).min(1).max(500).name("Power").description("Force to attack with").get());
 
     public ErrorCrash() {
-        super("BeeCrash", "Bee crash yours truly.", ModuleType.CRASH);
+        super("BeeCrash", "Bee crash yours truly. :)", ModuleType.CRASH);
     }
 
     @EventListener(type=EventType.PACKET_SEND)
