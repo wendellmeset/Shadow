3c6e3015f6bff2020bc5af291b77c67f13487a0a
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2022-10-10 09:14:25 -0700

    Update GameTexture.java

--- src/main/java/net/shadow/client/helper/GameTexture.java
+++ src/main/java/net/shadow/client/helper/GameTexture.java
@@ -7,28 +7,28 @@ package net.shadow.client.helper;
 import lombok.Getter;
 
 public enum GameTexture {
-    TEXTURE_ICON(new Texture("tex/icon"), "https://github.com/Saturn5Vfive/ShadowClient/blob/main/icon.png?raw=true"),
-    TEXTURE_BACKGROUND(new Texture("tex/background"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/background.jpg?inline=false"),
-    TEXTURE_LOGO(new Texture("tex/logo"), "https://github.com/Saturn5Vfive/ShadowClient/blob/main/shadow_logo.png?raw=true"),
-    TEXTURE_ICON_FULL(new Texture("tex/iconFull"), "https://github.com/Saturn5Vfive/ShadowClient/blob/main/shadowLogoFull.png?raw=true"),
+    TEXTURE_ICON(new Texture("tex/icon"), "assets/shadow/icon.png"),
+    TEXTURE_BACKGROUND(new Texture("tex/background"), "assets/shadow/background.jpg"),
+    TEXTURE_LOGO(new Texture("tex/logo"), "assets/shadow/shadow_logo.png"),
+    TEXTURE_ICON_FULL(new Texture("tex/iconFull"), "assets/shadow/shadowLogoFull.png"),
 
-    NOTIF_ERROR(new Texture("notif/error"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/error.png"),
-    NOTIF_INFO(new Texture("notif/info"), "https://github.com/Saturn5Vfive/ShadowClient/blob/main/appointment-reminders.png?raw=true"),
-    NOTIF_SUCCESS(new Texture("notif/success"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/success.png"),
-    NOTIF_WARNING(new Texture("notif/warning"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/warning.png"),
+    NOTIF_ERROR(new Texture("notif/error"), "assets/shadow/error.png"),
+    NOTIF_INFO(new Texture("notif/info"), "assets/shadow/info.png"),
+    NOTIF_SUCCESS(new Texture("notif/success"), "assets/shadow/success.png"),
+    NOTIF_WARNING(new Texture("notif/warning"), "assets/shadow/warning.png"),
 
-    ICONS_RENDER(new Texture("icons/render"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/render.png"),
-    ICONS_CRASH(new Texture("icons/crash"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/crash.png"),
-    ICONS_GRIEF(new Texture("icons/grief"), "https://github.com/Saturn5Vfive/monty/blob/main/icons8-spaceship-100.png?raw=true"),
-    ICONS_ADDON_PROVIDED(new Texture("icons/item"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/addons.png"),
-    ICONS_MOVE(new Texture("icons/move"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/movement.png"),
-    ICONS_MISC(new Texture("icons/misc"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/misc.png"),
-    ICONS_WORLD(new Texture("icons/world"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/world.png"),
-    ICONS_EXPLOIT(new Texture("icons/exploit"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/exploit.png"),
-    ICONS_COMBAT(new Texture("icons/combat"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/combat.png"),
+    ICONS_RENDER(new Texture("icons/render"), "assets/shadow/render.png"),
+    ICONS_CRASH(new Texture("icons/crash"), "assets/shadow/crash.png"),
+    ICONS_GRIEF(new Texture("icons/grief"), "assets/shadow/fun.png"),
+    ICONS_ADDON_PROVIDED(new Texture("icons/item"), "assets/shadow/addons.png"),
+    ICONS_MOVE(new Texture("icons/move"), "assets/shadow/movement.png"),
+    ICONS_MISC(new Texture("icons/misc"), "assets/shadow/misc.png"),
+    ICONS_WORLD(new Texture("icons/world"), "assets/shadow/world.png"),
+    ICONS_EXPLOIT(new Texture("icons/exploit"), "assets/shadow/exploit.png"),
+    ICONS_COMBAT(new Texture("icons/combat"), "assets/shadow/combat.png"),
 
-    ACTION_RUNCOMMAND(new Texture("actions/runCommand"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/command.png"),
-    ACTION_TOGGLEMODULE(new Texture("actions/toggleModule"), "https://gitlab.com/0x151/coffee-fs/-/raw/main/toggle.png");
+    ACTION_RUNCOMMAND(new Texture("actions/runCommand"), "assets/shadow/command.png"),
+    ACTION_TOGGLEMODULE(new Texture("actions/toggleModule"), "assets/shadow/toggle.png");
     @Getter
     final String downloadUrl;
     @Getter
