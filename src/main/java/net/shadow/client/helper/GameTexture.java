ccb59a17a05b42c4044de659a1d12ad5f5c8f470
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2022-10-11 16:10:02 -0700

    Update GameTexture.java

--- src/main/java/net/shadow/client/helper/GameTexture.java
+++ src/main/java/net/shadow/client/helper/GameTexture.java
@@ -19,13 +19,13 @@ public enum GameTexture {
 
     ICONS_RENDER(new Texture("icons/render"), "https://drive.google.com/uc?export=download&id=15XYCuH7tIi0pNbAi_FIwz7UjEIZdGX05"),
     ICONS_CRASH(new Texture("icons/crash"), "https://drive.google.com/uc?export=download&id=1jRD056Fom5-i9QNCuC3vBVBzUK1mhOYM"),
-    ICONS_GRIEF(new Texture("icons/grief"), "https://drive.google.com/uc?export=download&id=1mTm3rXF2FizdKYW_pjHi1biI9vVeAmkA"),
+    ICONS_GRIEF(new Texture("icons/grief"), "a"),
     ICONS_ADDON_PROVIDED(new Texture("icons/item"), "https://drive.google.com/uc?export=download&id=112HRiNkIv6DJLxBwVkNIjf6eL5oUtU9E"),
     ICONS_MOVE(new Texture("icons/move"), "https://drive.google.com/uc?export=download&id=12lgG1sa9pgcRJGiqmHRPlbgyipMzJeRj"),
     ICONS_MISC(new Texture("icons/misc"), "https://drive.google.com/uc?export=download&id=1JV-TCVH6epPPkre1bgp0clPfnt3jUZP5"),
     ICONS_WORLD(new Texture("icons/world"), "https://drive.google.com/uc?export=download&id=1rz35RXksiRCKW-h28PoDwx4x543H7Ujq"),
     ICONS_EXPLOIT(new Texture("icons/exploit"), "https://drive.google.com/uc?export=download&id=1cwPbPaq1v9CI4Oy0CP9JSkX8rZvYpXzq"),
-    ICONS_COMBAT(new Texture("icons/combat"), "https://drive.google.com/uc?export=download&id=14j2FklQBui8dz3IqY86CgwxvBvEUUd5j"),
+    ICONS_COMBAT(new Texture("icons/combat"), "a"),
 
     ACTION_RUNCOMMAND(new Texture("actions/runCommand"), "https://drive.google.com/uc?export=download&id=1Uh1YnOf8QaTM71P3cNHC0DhPgQAh-9uJ"),
     ACTION_TOGGLEMODULE(new Texture("actions/toggleModule"), "https://drive.google.com/uc?export=download&id=1euUtkGdJ2zTHAZ_h8uMxX4zXZAJ6nqzZ");
