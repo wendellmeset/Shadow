ba8279ae002eb5f39793117dd63e29c06a38f371
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2022-10-11 19:21:52 -0700

    Create ResourceTexture.java

---
+++ src/main/java/net/shadow/client/helper/ResourceTexture.java
@@ -0,0 +1,65 @@
+import net.shadow.client.ShadowMain;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class ResourceTexture {
+
+    // Stores paths to files with the global jarFilePath as the key
+    private static Hashtable<String, String> fileCache = new Hashtable<String, String>();
+
+    /**
+     * Extract the specified resource from inside the jar to the local file system.
+     * @param jarFilePath absolute path to the resource
+     * @return full file system path if file successfully extracted, else null on error
+     */
+    public static String extract(String jarFilePath){
+
+        if(jarFilePath == null)
+            return null;
+
+        // See if we already have the file
+        if(fileCache.contains(jarFilePath))
+            return fileCache.get(jarFilePath);
+
+        // Alright, we don't have the file, let's extract it
+        try {
+            // Read the file we're looking for
+            InputStream fileStream = ResourceManager.class.getResourceAsStream(jarFilePath);
+
+            // Was the resource found?
+            if(fileStream == null)
+                return null;
+
+            // Grab the file name
+            String[] chopped = jarFilePath.split("\\/");
+            String fileName = chopped[chopped.length-1];
+
+            // Create our temp file (first parameter is just random bits)
+            
+            File theFile = new File(ShadowMain.SHADOW_TEXTURING, fileName);
+
+            // Create an output stream to barf to the temp file
+            OutputStream out = new FileOutputStream(theFile);
+
+            // Write the file to the temp file
+            byte[] buffer = new byte[1024];
+            int len = fileStream.read(buffer);
+            while (len != -1) {
+                out.write(buffer, 0, len);
+                len = fileStream.read(buffer);
+            }
+
+            // Close the streams
+            fileStream.close();
+            out.close();
+
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
